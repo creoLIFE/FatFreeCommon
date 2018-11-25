2f7ac43c0ee13a73abb58ef0a1e3bc06eb6881f7
miro <mirek.ratman@gmail.com>
2018-11-26 00:11:23 +0100

    update function call

--- FatFree/Helpers/Url.php
+++ FatFree/Helpers/Url.php
@@ -86,7 +86,7 @@ class Url
      * @param string $string
      * @return mixed
      */
-    private function cleanString($string)
+    private static function cleanString($string)
     {
         //cyrylic transcription
         $cyrylicFrom = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
